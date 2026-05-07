c3efc3427288f09c5219de6de1cfe65acf9d1990
Jewelllllai <jewellaizs.0615@gmail.com>
2026-05-07 12:39:50 +0800

    feat: add /health endpoint for Docker and Azure healthchecks

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/health', (req, res) => res.sendStatus(200));
+
 function httpGet(url) {
   return new Promise((resolve, reject) => {
     https
