41c21dae84c273c8eb6211d55e6335b2f78a9ae5
pastadellacasaaa <104387846@students.swinburne.edu.my>
2026-05-14 20:42:24 +0800

    updated: test with webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },*/
 
-    {*/
+    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
